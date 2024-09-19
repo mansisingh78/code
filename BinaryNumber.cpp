a092053ac0097a860f8f628d27ef1ffcad6c731f
Mansi singh <mansi123singh1234@gmail.com>
2024-09-19 07:50:01 +0530

    completed

---
+++ BinaryNumber.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+// decimal to binary
+int decToBinary(int decNum)
+{
+    int ans = 0, pow = 1;
+
+    while (decNum > 0)
+    {
+        int rem = decNum % 2;
+        decNum /= 2;
+
+        ans += (rem * pow);
+        pow *= 10;
+    }
+    return ans;
+}
+int main()
+{
+    //     int decNum = 50;
+
+    //     cout << decToBinary(decNum) << endl;
+
+    for (int i = 1; i <= 10; i++)
+    {
+        cout << decToBinary(i) << endl;
+    }
+}
