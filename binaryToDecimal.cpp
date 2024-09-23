43d31588eb26feb133817bfcfe077293f0addaa6
Mansi singh <mansi123singh1234@gmail.com>
2024-09-23 18:21:40 +0530

    complete

--- binaryToDecimal.cpp
+++ binaryToDecimal.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 using namespace std;
 
-// Function to convert binary to decimal
 int binToDecimal(int binNum)
 {
     int ans = 0, pow = 1;
---
+++ practice2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float num1, num2;
+    cout << "enter a number:";
+    cin >> num1;
+    cout << "enter a number:";
+    cin >> num2;
+
+    float product = num1 * num2;
+    cout << "product = " << product << endl;
+}
